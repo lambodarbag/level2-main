d05bac73ddeb06228c38abe4bf08fef96507fbbe
Lambodar Bag <152137585+lambodarbag@users.noreply.github.com>
2024-03-22 00:58:23 +0530

    completed

--- pinterest/script.js
+++ pinterest/script.js
@@ -29,4 +29,29 @@ function showTheCards() {
     document.querySelector(".container").innerHTML = clutter;
 }
 
+
+function handleSearcFunctionality() {
+    var input = document.querySelector("#searchinput");
+
+    input.addEventListener("focus", function (){
+        document.querySelector(".overlay").style.display = "block";
+    })
+    input.addEventListener("blur", function (){
+        document.querySelector(".overlay").style.display = "none";
+    })
+    input.addEventListener("input", function() {
+        const filteredArray = arr.filter(obj => obj.name.toLowerCase().startsWith(input.value));
+        var clutter = " ";
+        filteredArray.forEach(function(obj){
+            clutter += `<div class="res flex px-8 py-3">
+            <i class="ri-search-line font-semibold mr-5"></i>
+            <h3 class="font-semibold">${obj.name}</h3>
+        </div>`;
+        })
+        document.querySelector(".searchdata").style.display = "block";
+        document.querySelector(".searchdata").innerHTML = clutter;
+    } )
+}
+
 showTheCards();
+handleSearcFunctionality();
